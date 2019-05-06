c29db2c5c585aced76e6b8a2ca74d05744622246
aparkening <aaron.parkening@gmail.com>
2019-05-06 13:52:20 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,25 @@
-# Your code goes here!
+# Define Dog Class
+class Dog
+
+  # Setting
+  def name=(dog_name)
+    @this_dogs_name = dog_name
+  end
+ 
+  # Getting
+  def name
+    @this_dogs_name
+  end
+
+  # Instance Method
+  def bark
+    puts "woof!"
+  end
+
+end
+
+fido = Dog.new
+fido.name = "Fido"
+
+fido.name
+fido.bark
